c08ec174861fabed7b36510bc750edfaeec2ebf4
chendongdong <564964001@qq.com>
2023-05-10 13:41:43 +0800

    1.1

--- bin/ts2js2minjs.js
+++ bin/ts2js2minjs.js
@@ -2,6 +2,9 @@
 
 const ts2js2minjs = require('../index');
 const path = require("path");
+const { merge } = require('lodash');
+const yargs = require('yargs');
+const {readFileSync,existsSync} = require("fs");
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
@@ -9,7 +12,37 @@ if (args.length === 0) {
     process.exit(1);
 }
 
-const tsPath = args[0];
+const argv = yargs.option('tsc_conf', {
+    alias: 'tsc',
+    type: 'string',
+    description: 'ts.config.json 文件路径, 参考 ts 配置文件',
+}).argv;
+
+const tsPath = path.normalize(args[0]);
+// 设置执行路径 默认为文档所在目录
 process.chdir(path.dirname(tsPath));
 
-ts2js2minjs(tsPath);
+// ts compilerOptions 默认参数
+const defTSOption = {
+    target: 'es6',
+    noImplicitAny: true,
+    sourceMap: true,
+    inlineSources: true
+}
+
+if (argv.tsc_conf){
+    let tsConfigPath = path.normalize(argv.tsc_conf);
+    //
+    if (!existsSync(tsConfigPath)){
+        console.error('tsc_conf 配置文件:',tsConfigPath,'不存在')
+        process.exit(1);
+    }
+    //
+    let tsOptions = tsConfigPath
+        ? merge(defTSOption, JSON.parse(readFileSync(tsConfigPath).toString()).compilerOptions)
+        : defTSOption;
+    console.log(tsOptions)
+    // ts2js2minjs(tsPath,tsOptions);
+}else {
+    ts2js2minjs(tsPath,defTSOption);
+}
